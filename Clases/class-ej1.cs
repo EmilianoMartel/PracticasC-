a160b0056d11c9950684c4f43e67c1dd316b9eb0
emiliano martel <46003816+EmilianoMartel@users.noreply.github.com>
2023-02-01 17:25:58 -0300

    clases

---
+++ Clases/class-ej1.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Confeccionar una clase que permita carga el nombre y la edad de una persona. Mostrar los datos cargados. Imprimir un mensaje si es mayor de edad (edad>=18)
+namespace Ejercicio1
+{
+    
+}
